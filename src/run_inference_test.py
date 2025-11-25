0d647d3fb8777034d6edb79f7edced0ecf013ad9
Aaron7317 <aaron.boyd.org@gmail.com>
2025-11-25 10:45:48 -0700

    misc changes

--- src/run_inference_test.py
+++ src/run_inference_test.py
@@ -7,7 +7,6 @@ from typing import List, Tuple, Union
 
 # Third Party
 import numpy as np
-from bokeh.io import export_png
 from bokeh.plotting import gridplot
 from PIL import Image
 
@@ -33,10 +32,7 @@ from megapose.visualization.utils import make_contour_overlay
 logger = get_logger(__name__)
 
 
-def load_observation(
-    example_dir: Path,
-    load_depth: bool = False,
-) -> Tuple[np.ndarray, Union[None, np.ndarray], CameraData]:
+def load_observation(example_dir: Path, load_depth: bool = False) -> Tuple[np.ndarray, Union[None, np.ndarray], CameraData]:
     camera_data = CameraData.from_json((example_dir / "camera_data.json").read_text())
 
     rgb = np.array(Image.open(example_dir / "image_rgb.png"), dtype=np.uint8)
@@ -50,10 +46,7 @@ def load_observation(
     return rgb, depth, camera_data
 
 
-def load_observation_tensor(
-    example_dir: Path,
-    load_depth: bool = False,
-) -> ObservationTensor:
+def load_observation_tensor(example_dir: Path, load_depth: bool = False) -> ObservationTensor:
     rgb, depth, camera_data = load_observation(example_dir, load_depth)
     observation = ObservationTensor.from_numpy(rgb, depth, camera_data.K)
     return observation
@@ -65,9 +58,7 @@ def load_object_data(data_path: Path) -> List[ObjectData]:
     return object_data
 
 
-def load_detections(
-    example_dir: Path,
-) -> DetectionsType:
+def load_detections(example_dir: Path) -> DetectionsType:
     input_object_data = load_object_data(example_dir / "inputs/object_data.json")
     detections = make_detections_from_object_data(input_object_data).cuda()
     return detections
@@ -91,25 +82,7 @@ def make_object_dataset(example_dir: Path) -> RigidObjectDataset:
     return rigid_object_dataset
 
 
-def make_detections_visualization(
-    example_dir: Path,
-) -> None:
-    rgb, _, _ = load_observation(example_dir, load_depth=False)
-    detections = load_detections(example_dir)
-    plotter = BokehPlotter()
-    fig_rgb = plotter.plot_image(rgb)
-    fig_det = plotter.plot_detections(fig_rgb, detections=detections)
-    output_fn = example_dir / "visualizations" / "detections.png"
-    output_fn.parent.mkdir(exist_ok=True)
-    # export_png(fig_det, filename=output_fn)
-    logger.info(f"Wrote detections visualization: {output_fn}")
-    return
-
-
-def save_predictions(
-    example_dir: Path,
-    pose_estimates: PoseEstimatesType,
-) -> None:
+def save_predictions(example_dir: Path, pose_estimates: PoseEstimatesType) -> None:
     labels = pose_estimates.infos["label"]
     poses = pose_estimates.poses.cpu().numpy()
     object_data = [
@@ -123,10 +96,7 @@ def save_predictions(
     return
 
 
-def run_inference(
-    example_dir: Path,
-    model_name: str,
-) -> None:
+def run_inference(example_dir: Path, model_name: str) -> None:
 
     model_info = NAMED_MODELS[model_name]
 
@@ -148,81 +118,17 @@ def run_inference(
     return
 
 
-def make_output_visualization(
-    example_dir: Path,
-) -> None:
-
-    rgb, _, camera_data = load_observation(example_dir, load_depth=False)
-    camera_data.TWC = Transform(np.eye(4))
-    object_datas = load_object_data(example_dir / "outputs" / "object_data.json")
-    object_dataset = make_object_dataset(example_dir)
-
-    renderer = Panda3dSceneRenderer(object_dataset)
-
-    camera_data, object_datas = convert_scene_observation_to_panda3d(camera_data, object_datas)
-    light_datas = [
-        Panda3dLightData(
-            light_type="ambient",
-            color=((1.0, 1.0, 1.0, 1)),
-        ),
-    ]
-    renderings = renderer.render_scene(
-        object_datas,
-        [camera_data],
-        light_datas,
-        render_depth=False,
-        render_binary_mask=False,
-        render_normals=False,
-        copy_arrays=True,
-    )[0]
-
-    plotter = BokehPlotter()
-
-    fig_rgb = plotter.plot_image(rgb)
-    fig_mesh_overlay = plotter.plot_overlay(rgb, renderings.rgb)
-    contour_overlay = make_contour_overlay(
-        rgb, renderings.rgb, dilate_iterations=1, color=(0, 255, 0)
-    )["img"]
-    fig_contour_overlay = plotter.plot_image(contour_overlay)
-    fig_all = gridplot([[fig_rgb, fig_contour_overlay, fig_mesh_overlay]], toolbar_location=None)
-    vis_dir = example_dir / "visualizations"
-    vis_dir.mkdir(exist_ok=True)
-    # export_png(fig_mesh_overlay, filename=vis_dir / "mesh_overlay.png")
-    # export_png(fig_contour_overlay, filename=vis_dir / "contour_overlay.png")
-    # export_png(fig_all, filename=vis_dir / "all_results.png")
-    logger.info(f"Wrote visualizations to {vis_dir}.")
-    return
-
-
-# def make_mesh_visualization(RigidObject) -> List[Image]:
-#     return
-
-
-# def make_scene_visualization(CameraData, List[ObjectData]) -> List[Image]:
-#     return
-
-
-# def run_inference(example_dir, use_depth: bool = False):
-#     return
-
-
 if __name__ == "__main__":
     set_logging_level("info")
     parser = argparse.ArgumentParser()
     parser.add_argument("example_name")
     parser.add_argument("--model", type=str, default="megapose-1.0-RGB-multi-hypothesis")
-    parser.add_argument("--vis-detections", action="store_true")
     parser.add_argument("--run-inference", action="store_true")
-    parser.add_argument("--vis-outputs", action="store_true")
     args = parser.parse_args()
 
-    example_dir = LOCAL_DATA_DIR / "examples" / args.example_name
-
-    if args.vis_detections:
-        make_detections_visualization(example_dir)
-
+    #example_dir = LOCAL_DATA_DIR / "examples" / args.example_name
     if args.run_inference:
-        run_inference(example_dir, args.model)
+        run_inference(Path(args.example_name), args.model)
 
-    if args.vis_outputs:
-        make_output_visualization(example_dir)
+    #if args.run_inference:
+    #    run_inference(example_dir, args.model)
